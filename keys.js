18b4953ca8874fe76982b27961a0d417cac68791
alex-bowen <alexbowen2017@gmail.com>
2018-08-25 17:15:17 -0500

    Initial commit

--- keys.js
+++ keys.js
@@ -1,5 +1,6 @@
 console.log('this is loaded');
 
 exports.spotify = {
-  id: process.env.SPOTIFY_ID,
-  secret: process.env.SPOTIFY_SECRET
+  id: process.env.b7929ed7174b4eb7a731a58a2e7a7c7f,
+  secret: process.env.119f4f9ceb724798882faa458a2033e2
+};
--- liri.js
+++ liri.js
@@ -1,2 +1,31 @@
 require("dotenv").config();
-require("keys");
+
+// Add the code required to import the `keys.js` file and store it in a variable.
+  
+// * You should then be able to access your keys information like so
+
+//   ```js
+//   var spotify = new Spotify(keys.spotify);
+//   ```
+
+
+
+// Make it so liri.js can take in one of the following commands:
+
+// * `concert-this`
+
+// * `spotify-this-song`
+
+// * `movie-this`
+
+// * `do-what-it-says`
+
+
+
+// OMDB API key 4ff3048d
+// OMDb API: http://www.omdbapi.com/?i=tt3896198&apikey=4ff3048d
+
+
+
+// Bandsintown
+// var queryURL = "https://rest.bandsintown.com/artists/" + artist + "?app_id=codingbootcamp";
